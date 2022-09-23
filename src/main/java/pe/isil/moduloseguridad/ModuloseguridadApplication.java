454979721d53e965d3d73b22c436e4524a6ef16e
romahhzg <romahhzg@gmail.com>
2022-09-22 21:03:26 -0500

    First commit

---
+++ src/main/java/pe/isil/moduloseguridad/ModuloseguridadApplication.java
@@ -0,0 +1,15 @@
+package pe.isil.moduloseguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloseguridadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuloseguridadApplication.class, args);
+
+		System.out.print("Hola mundo");
+	}
+
+}
---
+++ src/test/java/pe/isil/moduloseguridad/ModuloseguridadApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.isil.moduloseguridad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuloseguridadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
